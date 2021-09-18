b40259a93651a5c193a66b22d0e03449f2c2b0d8
McHippy3 <dwu2010@live.ca>
2021-09-18 02:41:50 -0400

    pose only

--- backend/script.py
+++ backend/script.py
@@ -5,7 +5,7 @@ import numpy as np
 import math
 
 mp_drawing = mp.solutions.drawing_utils
-mp_holistic = mp.solutions.holistic
+mp_pose = mp.solutions.pose
 
 VIDEO_NAME = 'Workout Dance'
 VIDEO_PATH = 'videos/workout_vid.mp4'
@@ -22,22 +22,29 @@ wc.set(4, WIDTH)
 vid.set(3, HEIGHT)
 vid.set(4, WIDTH)
 
+last_time = 0
+
 # Main loop
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic_wc, \
-     mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic_vid:
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic_wc, \
+     mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic_vid:
     while wc.isOpened() and vid.isOpened():
+        # Framerate
+        current_time = time.time()
+        fps = 1/(current_time-last_time)
+        last_time = current_time
+        print(fps)
+
         # Webcam
-        # Limit framerate to 15 fps
         wc_success, wc_frame = wc.read()
         image = cv2.cvtColor(wc_frame, cv2.COLOR_BGR2RGB)
         try:
             results = holistic_wc.process(image)
         except Exception as e:
-            holistic_wc= mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+            holistic_wc= mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         # Draw pose
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
             mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
             mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
         
@@ -50,11 +57,11 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         try:
             results = holistic_vid.process(image)
         except Exception as e:
-            holistic_vid = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+            holistic_vid = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         # Draw pose
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
             mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
             mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
         
