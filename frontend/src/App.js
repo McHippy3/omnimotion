3d3c4f32dfc0b946087401a46b30f732716663e9
Jesse Liu <42053831+jasssse@users.noreply.github.com>
2021-09-19 01:32:08 -0400

    centered stuff lol

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Button from '@mui/material/Button';
 import './App.css';
 
@@ -6,30 +5,40 @@ function App() {
   return (
     <div className="App">
       <header className="box">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ color: '#F0F4EF' }}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
 
-        <div style={{ marginBottom: 15 }}>
-          <Button variant="contained" className="uploadButton" style={{ backgroundColor: "#F26430" }} component="label">
-            Upload File
-            <input
-              type="file"
-              accept=".mp4"
-            hidden
-            />
-          </Button>
+        <div style={{ flex: 0.2, alignItems: 'center'}}>
+          {/*TODO: Add logo*/}
+
+          <h1 style={{ color: '#F0F4EF' }}>
+            OmniMotion
+          </h1>
         </div>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div style={{display: 'flex', flex: 0.8, flexDirection: 'row'}}>
+          {/*COL 1*/}
+          <div style={{ flex: 0.5}}>
+
+          </div>
+
+
+          {/*COL 2*/}
+          <div style={{display: 'flex', flex: 0.5, alignItems: 'center', justifyContent: 'center'}}>
+          <Button variant="contained" className="uploadButton" style={{ backgroundColor: "#F26430", width: '30%'}} component="label">
+                Upload File
+                <input
+                  type="file"
+                  accept=".mp4"
+                  hidden
+                />
+              </Button>
+            
+          </div>
+
+
+
+        </div>
+
       </header>
     </div>
   );
