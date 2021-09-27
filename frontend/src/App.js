fad4e9fdc0a4b9ff9ccf27abdffd24e2b44e8b7f
McHippy3 <dwu2010@live.ca>
2021-09-27 00:21:36 -0400

    layout changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import useInterval from './useInterval';
 import ReactPlayer from "react-player";
 import Button from '@mui/material/Button';
@@ -16,31 +16,46 @@ function App() {
   const [videoFilePath, setVideoFilePath] = useState(null);
   const [videoPlaying, setVideoPlaying] = useState(false);
   const [perfVal, setPerfVal] = useState(0);
+  const [wcFlex, setWCFlex] = useState(0.5);
+  const [vidFlex, setVidFlex] = useState(0.5);
   const videoPlayer = useRef(null);
 
   const socket = socketIOClient(ENDPOINT);
 
+  useEffect(() => {
+    socket.on('performance', newPerfVal => {
+      setPerfVal(newPerfVal);
+    });
+  }, []);
+
   useInterval(() => {
     if (videoPlaying) {
       try {
+        const wc_frame = webcamRef.current.getScreenshot();
         const vid_frame = captureVideoFrame(videoPlayer.current.getInternalPlayer(), 'jpeg').dataUri;
-        socket.emit('new_frame', vid_frame);
+
+        // Downscale video frame using offscreen canvas
+        var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+
+        canvas.width = 768;
+        canvas.height = 480;
+
+        ctx.drawImage(vid_frame, 0, 0, 768, );
+
+        socket.emit('new_frame_wc', wc_frame);
+        socket.emit('new_frame_vid', canvas.toDataURL('image/jpeg'));
       } catch (err) {
+        console.log(err)
       }
     }
-    else {
-      const wc_frame = webcamRef.current.getScreenshot();
-      socket.emit('new_frame', wc_frame);
-    }
-  }, 67);
-
-  // socket.on('performance', x => {
-  //   setPerfVal(x)
-  // });
+  }, 1000);
 
   const handleVideoUpload = (event) => {
     setVideoFilePath(URL.createObjectURL(event.target.files[0]));
     setVideoPlaying(true);
+    setWCFlex(0.3);
+    setVidFlex(0.7)
   }
 
   return (
@@ -58,15 +73,15 @@ function App() {
 
         <div style={{ display: 'flex', flex: 0.8, flexDirection: 'row' }}>
           {/*COL 1*/}
-          <div style={{ flex: 0.5 }}>
-            <Webcam style={{ borderRadius: 10, boxShadow: "0px 0px 70px red" }} ref={webcamRef} />
+          <div style={{ flex: wcFlex, alignItems: 'center'}}>
+            <Webcam style={{ maxWidth: "80%", borderRadius: 10, boxShadow: "0px 0px 70px red" }} ref={webcamRef} />
           </div>
 
 
           {/*COL 2*/}
-          <div style={{ display: 'flex', flexDirection: 'column', flex: 0.5, alignItems: 'center', justifyContent: 'space-evenly' }}>
-            <div style={{ flex: 0.8, display: videoPlaying ? "": "none" }}>
-              <ReactPlayer id="video-file" ref={videoPlayer} url={videoFilePath} playing={videoPlaying} controls={true} />
+          <div style={{ display: 'flex', flexDirection: 'column', flex: vidFlex, alignItems: 'center', justifyContent: 'space-evenly' }}>
+            <div style={{ display: videoPlaying ? '' : 'none' }}>
+              <ReactPlayer id="video-file" width={1152} height={720} ref={videoPlayer} url={videoFilePath} playing={videoPlaying} controls={true} />
             </div>
             <Button variant="contained" className="uploadButton" style={{ backgroundColor: "#F26430", width: '30%', display: videoPlaying ? "none" : "" }} component="label">
               Upload File
